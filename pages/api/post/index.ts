a1247bf3d449a9083fa1e423273f3fba3e7e5655
PraveenNarwal <praveennarwal5@gmail.com>
2022-07-03 11:14:16 +0530

    sanity setup cors and api console

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,12 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { allPostsQuery } from "../../../utils/queries";
+import { client } from "../../../utils/client";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "Response getting...." });
+  if (req.method === "GET") {
+    const query = allPostsQuery();
+    const data = await client.fetch(query);
+
+    res.status(200).json(data);
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import axios from "axios";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ video }) => {
+  console.log(video);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
-    props: {},
+    props: {
+      video: data,
+    },
   };
 };
 
